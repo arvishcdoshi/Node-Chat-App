924f3fc1a55639a78e4901e6bcd07e38296f8568
Arvish Doshi <doshi.arvish16@gmail.com>
2026-02-27 20:19:26 +0530

    Added timestamps in the Location sharing logic

--- public/js/chat.js
+++ public/js/chat.js
@@ -26,7 +26,7 @@ const locationMessageTemplate = document.querySelector('#location-message-templa
 // })
 
 socket.on('message', (message) => {
-    console.log(message)
+    // console.log(message)
     const html = Mustache.render(messageTemplate, {
         message: message.text,
         createdAt: moment(message.createdAt).format('h:mm a')
@@ -34,10 +34,11 @@ socket.on('message', (message) => {
     $messages.insertAdjacentHTML('beforeend', html)
 })
 
-socket.on('locationMessage', (url) => {
+socket.on('locationMessage', (message) => {
     // console.log(url)
     const html = Mustache.render(locationMessageTemplate, {
-        url
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
