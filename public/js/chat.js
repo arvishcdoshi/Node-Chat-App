1e96be35247d0d64a8d615d26f1481839c70443e
Arvish Doshi <doshi.arvish16@gmail.com>
2026-02-28 13:03:03 +0530

    Sending messages to rooms & template modififcation for system & user messages.

--- public/js/chat.js
+++ public/js/chat.js
@@ -33,6 +33,7 @@ const { username, room } = qs.parse(location.search, { ignoreQueryPrefix: true }
 socket.on('message', (message) => {
     // console.log(message)
     const html = Mustache.render(messageTemplate, {
+        username: message.username,
         message: message.text,
         createdAt: moment(message.createdAt).format('h:mm a')
     })
@@ -42,6 +43,7 @@ socket.on('message', (message) => {
 socket.on('locationMessage', (message) => {
     // console.log(url)
     const html = Mustache.render(locationMessageTemplate, {
+        username: message.username,
         url: message.url,
         createdAt: moment(message.createdAt).format('h:mm a')
     })
